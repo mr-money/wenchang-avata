3693891f39cf16f558b24ad9e402264d9da137c1
qianjing <740363495@qq.com>
2022-10-09 09:59:35 +0800

    demo编写创建链账户

--- Demo.php
+++ Demo.php
@@ -2,7 +2,34 @@
 
 namespace Demo;
 
+use AvataLogic\AvataLogic;
+
 class Demo
 {
+    public $AvataLogic;
 
+    public function __construct()
+    {
+        $initData = [
+            'apiKey' => 'apiKey',
+            'apiSecret' => 'apiSecret',
+            'domain' => 'https://stage.apis.avata.bianjie.ai', //测试地址
+        ];
+        $this->AvataLogic = new AvataLogic($initData);
+
+    }
+
+
+    /**
+     * @description 创建链账户
+     * @return array
+     */
+    public function createAccount(): array
+    {
+        $name = 'account_name';
+        $operationId = 'operationId'.$this->AvataLogic->getMillisecond();
+
+        return $this->AvataLogic->CreateChainAccount($name,$operationId);
+
+    }
 }
