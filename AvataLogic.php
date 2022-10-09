e5b55cca36b93435b436c643696046d0d4b371b8
qianjing <740363495@qq.com>
2022-10-09 09:23:35 +0800

    init commit

---
+++ AvataLogic.php
@@ -0,0 +1,431 @@
+<?php
+
+/**
+ *
+ * @description 文昌链Avata API 快速接入SDK
+ *
+ * @author Mr_money
+ *
+ * https://apis.avata.bianjie.ai/
+ */
+
+namespace AvataLogic;
+
+class AvataLogic
+{
+    private $apiKey;
+    private $apiSecret;
+    private $domain;
+
+    public function __construct($data)
+    {
+        $this->apiKey = $data['api_key'] ?? '';
+        $this->apiSecret = $data['api_secret'] ?? '';
+        $this->domain = $data['api_url'] ?? 'https://stage.apis.avata.bianjie.ai'; //测试地址
+    }
+
+    /**
+     * @description 创建链账户
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/%E9%93%BE%E8%B4%A6%E6%88%B7%E6%8E%A5%E5%8F%A3/paths/~1v1beta1~1account/post
+     *
+     * @param string $name 链账户名称，支持 1-20 位汉字、大小写字母及数字组成的字符串
+     * @param string $operationId 操作ID
+     *
+     * @return array
+     */
+    public function CreateChainAccount(string $name, string $operationId): array
+    {
+        $body = [
+            'name' => $name,
+            'operation_id' => $operationId,
+        ];
+        return $this->request('/v1beta1/account', [], $body, 'POST');
+    }
+
+
+    /**
+     * @description 根据账户操作id 查询单个创建会员状态
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/%E9%93%BE%E8%B4%A6%E6%88%B7%E6%8E%A5%E5%8F%A3/paths/~1v1beta1~1accounts/get
+     *
+     * @param string $operationId 操作ID
+     *
+     * @return array
+     */
+    public function QueryAccountByOperationId(string $operationId): array
+    {
+        $query = [
+            'operation_id' => $operationId,
+        ];
+        return $this->request('/v1beta1/accounts', $query, [], 'GET');
+    }
+
+    /**
+     * @description 根据链账户名称 查询单个创建会员状态
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/%E9%93%BE%E8%B4%A6%E6%88%B7%E6%8E%A5%E5%8F%A3/paths/~1v1beta1~1accounts/get
+     *
+     * @param string $name 链账户名称，支持模糊查询
+     *
+     * @return array
+     */
+    public function QueryAccountByName(string $name): array
+    {
+        $query = [
+            'name' => $name,
+        ];
+        return $this->request('/v1beta1/accounts', $query, [], 'GET');
+    }
+
+    /**
+     * @description 根据条件查询链账户信息
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/%E9%93%BE%E8%B4%A6%E6%88%B7%E6%8E%A5%E5%8F%A3/paths/~1v1beta1~1accounts/get
+     *
+     * @param array $data [
+     * 'offset' => (string)'游标，默认为 0',
+     * 'limit' => (string)'每页记录数，默认为 10，上限为 50',
+     * 'account' => (string)'链账户地址',
+     * 'name' => (string)'链账户名称，支持模糊查询',
+     * 'operation_id' => (string)'操作 ID',
+     * 'start_date' => (string)'创建日期范围 - 开始，yyyy-MM-dd（UTC 时间）',
+     * 'end_date' => (string)'创建日期范围 - 结束，yyyy-MM-dd（UTC 时间）',
+     * 'sort_by' => (string)'排序规则：DATE_ASC / DATE_DESC',
+     * ]
+     *
+     * @return array
+     */
+    public function QueryAccounts(array $data): array
+    {
+        $query = [
+            'offset' => $data['offset'],
+            'limit' => $data['limit'],
+            'account' => $data['account'],
+            'name' => $data['name'],
+            'operation_id' => $data['operation_id'],
+            'start_date' => $data['start_date'],
+            'end_date' => $data['end_date'],
+            'sort_by' => $data['sort_by'],
+        ];
+        return $this->request('/v1beta1/accounts', $query, [], 'GET');
+    }
+
+
+    /**
+     * @description 上链交易结果查询
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/%E4%BA%A4%E6%98%93%E7%BB%93%E6%9E%9C%E6%9F%A5%E8%AF%A2%E6%8E%A5%E5%8F%A3/paths/~1v1beta1~1tx~1{operation_id}/get
+     *
+     * @param string $operationId 操作ID
+     *
+     * @return array
+     */
+    public function OperationResult(string $operationId): array
+    {
+        return $this->request('/v1beta1/tx/' . $operationId, [], [], 'GET');
+    }
+
+    //
+
+    /**
+     * @description 创建NFT类别
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1classes/post
+     *
+     * @param array $data [
+     * 'name' => 'NFT 类别名称',
+     * 'class_id' => 'NFT 类别 ID，仅支持小写字母及数字，以字母开头',
+     * 'owner' => 'NFT 类别权属者地址 链账户地址account',
+     * 'description' => '描述',
+     * 'tag' => (array)[ //交易标签
+     *              'key1' => 'value1'
+     *          ],
+     * ];
+     * @param $operationId
+     *
+     * @return array
+     */
+    public function CreateClasses($data, $operationId): array
+    {
+        $body = [
+            'name' => $data['name'],
+            'class_id' => $data['class_id'],
+            'owner' => $data['owner'],
+            'description' => $data['description'],
+            'tag' => $data['tag'],
+            'operation_id' => $operationId,
+        ];
+        return $this->request('/v1beta1/nft/classes', [], $body, 'POST');
+    }
+
+
+    /**
+     * @description 查询NFT类别详情
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1classes~1{id}/get
+     *
+     * @param string $classId
+     *
+     * @return array
+     */
+    public function QueryClassesDetail(string $classId): array
+    {
+        $query = [
+            'id' => $classId,
+        ];
+        return $this->request('/v1beta1/nft/classes', $query, [], 'GET');
+    }
+
+
+    /**
+     * @description 转让NFT类别
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1class-transfers~1{class_id}~1{owner}/post
+     *
+     * @param array $data
+     * @param string $operationId
+     *
+     * @return array
+     */
+    public function TransfersClasses(array $data, string $operationId): array
+    {
+        $body = [
+            'recipient' => $data['recipient'],
+            'operation_id' => $operationId,
+            'tag' => $data['tag'] ?: '',
+        ];
+        return $this->request('/v1beta1/nft/class-transfers/' . $data['class_id'] . '/' . $data['owner'], [], $body, 'POST');
+    }
+
+    /**
+     * @description 发行NFT
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nfts~1{class_id}/post
+     *
+     * @param array $data
+     * @param string $operationId
+     *
+     * @return array
+     */
+    public function CreateNft(array $data, string $operationId): array
+    {
+        $body = [
+            'name' => $data['name'],
+            'uri' => $data['uri'] ?: '',
+            'recipient' => $data['recipient'],
+            'operation_id' => $operationId,
+        ];
+        if (!empty($data['uri_hash'])) {
+            $body['uri_hash'] = $data['uri_hash'];
+        }
+        if (!empty($data['data'])) {
+            $body['data'] = $data['data'];
+        }
+        if (!empty($data['tag'])) {
+            $body['tag'] = $data['tag'];
+        }
+        return $this->request('/v1beta1/nft/nfts/' . $data['class_id'], [], $body, 'POST');
+    }
+
+    /**
+     * @description 转让NFT
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nft-transfers~1{class_id}~1{owner}~1{nft_id}/post
+     *
+     * @param array $data
+     * @param string $operationId
+     *
+     * @return array
+     */
+    public function TransfersNft(array $data, string $operationId): array
+    {
+        $body = [
+            'recipient' => $data['recipient'],
+            'operation_id' => $operationId,
+            'tag' => !empty($data['tag']) ? $data['tag'] : '',
+        ];
+        return $this->request('/v1beta1/nft/nft-transfers/' . $data['class_id'] . '/' . $data['owner'] . '/' . $data['nft_id'], [], $body, 'POST');
+    }
+
+
+    /**
+     * @description 编辑NFT
+     *
+     * @api  https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nfts~1{class_id}~1{owner}~1{nft_id}/patch
+     *
+     * @param array $data
+     * @param string $operationId
+     *
+     * @return array
+     */
+    public function EditNft(array $data, string $operationId): array
+    {
+        $body = [
+            'name' => $data['name'],
+            'uri' => !empty($data['uri']) ? $data['uri'] : '',
+            'data' => !empty($data['data']) ? $data['data'] : '',
+            'operation_id' => $operationId,
+            'tag' => !empty($data['tag']) ? $data['tag'] : '',
+        ];
+
+        return $this->request('/v1beta1/nft/class-transfers/' . $data['class_id'] . '/' . $data['owner'] . '/' . $data['nft_id'], [], $body, 'PATCH');
+    }
+
+
+    /**
+     * @description 销毁NFT
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nfts~1{class_id}~1{owner}~1{nft_id}/delete
+     *
+     * @param array $data
+     * @param string $operationId
+     *
+     * @return array
+     */
+    public function DeleteNft(array $data, string $operationId): array
+    {
+        $body = [
+            'operation_id' => $operationId,
+            'tag' => !empty($data['tag']) ? $data['tag'] : '',
+        ];
+
+        return $this->request('/v1beta1/nft/class-transfers/' . $data['class_id'] . '/' . $data['owner'] . '/' . $data['nft_id'], [], $body, 'DELETE');
+    }
+
+    /**
+     * @description 查询NFT列表
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nfts/get
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    public function QueryNftList(array $data): array
+    {
+        $query = [
+            'class_id' => $data['class_id'],
+            'owner' => $data['owner'],
+            'name' => $data['name'],
+            'status' => $data['status'],
+        ];
+        return $this->request('/v1beta1/nft/nfts/', $query, [], 'GET');
+    }
+
+    /**
+     * @description 查询NFT详情
+     *
+     * @api https://apis.avata.bianjie.ai/#tag/NFT/paths/~1v1beta1~1nft~1nfts~1{class_id}~1{nft_id}/get
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    public function QueryNftDetail(array $data): array
+    {
+        $query = [
+            'class_id' => $data['class_id'],
+            'nft_id' => $data['nft_id'],
+        ];
+        return $this->request('/v1beta1/nft/nfts/' . $data['class_id'] . '/' . $data['nft_id'], $query, [], 'GET');
+    }
+
+
+
+    ///////////////////////////////////////
+    /// function
+
+    /**
+     * @description 发送请求
+     *
+     * @param string $path
+     * @param array $query
+     * @param array $body
+     * @param string $method
+     *
+     * @return array
+     */
+    private function request(string $path, array $query = [], array $body = [], string $method = 'GET')
+    {
+        $method = strtoupper($method);
+        $apiGateway = rtrim($this->domain, '/') . '/' . ltrim($path,
+                '/') . ($query ? '?' . http_build_query($query) : '');
+        $timestamp = $this->getMillisecond();
+        $params = ['path_url' => $path];
+        if ($query) {
+            foreach ($query as $k => $v) {
+                $params['query_' . $k] = $v;
+            }
+        }
+        if ($body) {
+            foreach ($body as $k => $v) {
+                $params['body_' . $k] = $v;
+            }
+        }
+        ksort($params);
+        $hexHash = hash('sha256', $timestamp . $this->apiSecret);
+        if (count($params) > 0) {
+            $s = json_encode($params, JSON_UNESCAPED_UNICODE);
+            $hexHash = hash('sha256', stripcslashes($s . $timestamp . $this->apiSecret));
+        }
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $apiGateway);
+        $header = [
+            'Content-Type:application/json',
+            'X-Api-Key:' . $this->apiKey,
+            'X-Signature:' . $hexHash,
+            'X-Timestamp:' . $timestamp,
+        ];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $jsonStr = $body ? json_encode($body) : ''; //转换为json格式
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            if ($jsonStr) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+            }
+        } elseif ($method == 'PATCH') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            if ($jsonStr) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+            }
+        } elseif ($method == 'PUT') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            if ($jsonStr) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+            }
+        } elseif ($method == 'DELETE') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            if ($jsonStr) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+            }
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response_data = curl_exec($ch);
+
+        //$err = curl_error($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        $response_data = json_decode($response_data, true);
+
+        $response = array(
+            'code' => $httpCode,
+            'result' => $response_data,
+        );
+        return $response;
+    }
+
+    /** get timestamp
+     *
+     * @return float
+     */
+    private function getMillisecond()
+    {
+        list($t1, $t2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($t1) + floatval($t2)));
+    }
+
+}
